e4e894ccb2f7569f801c5bed9f7e5cca1b108aee
Sushmit Dharurkar <sushmit.sushmit@gmail.com>
2017-03-31 13:34:26 -0400

    added genres to movies

--- index.js
+++ index.js
@@ -96,9 +96,32 @@ app.post('/ai', (req, res) => {	//apiai requires json format return
 		results = body.results[0]
 		image = results.poster_path
 		image_url = "https://image.tmdb.org/t/p/w500" + image 
-		text = "Title: " + results.title + "\nRelease Date: " + results.release_date + "\nRating: " 
-					+ results.vote_average + "/10" + "\nSummary: "+results.overview
-		text = text.substr(0,639)
+		text = "Release Date: " + results.release_date + "\nRating: " 
+					+ results.vote_average + "/10" + "\nGenre: "//+ "\nSummary: "+results.overview
+		//text = text.substr(0,639)
+		
+		var genre_ids = results.genre_ids
+		var c = 0
+		var genre_names = []
+		/*Check if call is optimum or this loop*/
+		for (var i=0; i< genre_ids.length ; i++){
+			for (var j=0; j< genres_list.length ; j++){
+				if (genres_list[j].id == genre_ids[i]){
+					genre_names[c++] = genres_list[j].name
+					break
+				}
+			}
+		}	
+		
+		for (i=0;i<genre_names.length;i++){
+			if (i == genre_names.length - 1 ){
+				text += genre_names[i]
+			}
+			else{
+				text += genre_names[i] + ", "	
+			}
+		}
+		
 		attachData = {
 				type : 	"template",
 				payload : {
@@ -221,6 +244,15 @@ function requestPromise(url, json){	//Generic function for making GET requests t
 		})	
 	})
 }
+
+var genres_list = [
+	{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},
+	{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},
+	{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},
+	{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},
+	{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}
+]
+
 
 	 /*else {
         return res.status(400).json({
